0a0acdd3512e19bfb29a131ec0f2b936ac20ca57
Matt Swezey <matt@swezey.io>
2020-03-05 13:51:45 -0600

    Update shift_manager.bash

--- shift_manager.bash
+++ shift_manager.bash
@@ -15,7 +15,7 @@ set_network() {
   if [ "$(grep "7337a324ef27e1e234d1e9018cacff7d4f299a09c2df9be460543b8f7ef652f1" $SHIFT_CONFIG )" ];then
     NETWORK="mainnet"
   elif [ "$(grep "cba57b868c8571599ad594c6607a77cad60cf0372ecde803004d87e679117c12" $SHIFT_CONFIG )" ];then
-    NETWORK="test"
+    NETWORK="testnet"
   else
     NETWORK="unknown"
   fi
